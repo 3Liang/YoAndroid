e54a901e71011dad273f89d9cd00376017fca11b
Sam <leiyu@ultralinked.com>
2019-11-05 15:49:44 +0800

    1.0.0版本
    底部tab图标以及个人页面bug修改

--- pages/me/me.js
+++ pages/me/me.js
@@ -6,14 +6,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-    user: getApp().globalData.user
+    user: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-
+    this.setData({
+      user: getApp().globalData.user
+    })
   },
 
   /**
